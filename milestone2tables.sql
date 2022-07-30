f05e0480ca3bc8749359c8a2918a366722dc1feb
UBC Student <medlin@students.cs.ubc.ca>
2022-07-30 13:22:06 -0700

    Added .sql files
    Added .sql files to create tables and populate them with tuples

---
+++ milestone2tables.sql
@@ -0,0 +1,96 @@
+CREATE TABLE Listener(UserID INTEGER PRIMARY KEY,
+                    Email VARCHAR(50),
+                    ListeningTime INTEGER,
+                    TopGenre VARCHAR(20));
+
+CREATE TABLE Artist(UserID INTEGER PRIMARY KEY,
+                    Email VARCHAR(50),
+                    MonthlyListeners INTEGER);
+
+CREATE TABLE ProfileHas(UserID INTEGER,
+                        Nickname VARCHAR(30),
+                        PRIMARY KEY (UserID, Nickname),
+                        FOREIGN KEY (UserID) REFERENCES Listener);                                                                        
+
+CREATE TABLE Song(SongID INTEGER PRIMARY KEY,
+                    Genre VARCHAR(20),
+                    Duration REAL,
+                    Lyrics VARCHAR(200),
+                    NumStreams INTEGER,
+                    FOREIGN KEY (Lyrics) REFERENCES LyricsTitle(Lyrics),
+                    FOREIGN KEY (NumStreams) REFERENCES StreamSongCertification(NumStreams));
+
+CREATE TABLE UserLikesSong(UserID INTEGER,
+                            SongID INTEGER,
+                            PRIMARY KEY (UserID, SongID),
+                            FOREIGN KEY (UserID) REFERENCES Listener,
+                            FOREIGN KEY (SongID) REFERENCES Song); 
+
+CREATE TABLE StreamSongCertification(NumStreams INTEGER PRIMARY KEY,
+                                Certification VARCHAR(20));
+
+CREATE TABLE LyricsTitle(Lyrics VARCHAR(200) PRIMARY KEY,
+                        Title VARCHAR(200));
+
+CREATE TABLE SongHasKeyword(KeywordID INTEGER PRIMARY KEY,
+                            Keyword VARCHAR(30),
+                            SongID INTEGER,
+                            FOREIGN KEY (SongID) REFERENCES Song);
+
+CREATE TABLE UserPlaylists(PlaylistID INTEGER PRIMARY KEY);
+
+CREATE TABLE UserHasUserPlaylists(UserID INTEGER,
+                                PlaylistID INTEGER,
+                                PRIMARY KEY (UserID, PlaylistID),
+                                FOREIGN KEY (UserID) REFERENCES Listener,
+                                FOREIGN KEY (PlaylistID) REFERENCES UserPlaylists);
+
+CREATE TABLE PlaylistContent(ContentID INTEGER PRIMARY KEY,
+                            PlaylistID INTEGER,
+                            UNIQUE (PlaylistID),
+                            FOREIGN KEY (PlaylistID) REFERENCES UserPlaylists);
+
+CREATE TABLE PlaylistContentIncludesSong(ContentID INTEGER,
+                                        SongID INTEGER,
+                                        PRIMARY KEY (ContentID, SongID),
+                                        FOREIGN KEY (ContentID) REFERENCES PlaylistContent,
+                                        FOREIGN KEY (SongID) REFERENCES Song);   
+
+CREATE TABLE StreamAlbumCertification(NumStreams INTEGER PRIMARY KEY,
+                                    Certification VARCHAR(30));
+
+CREATE TABLE TitleRelease(Title VARCHAR(30),
+                            AlbumVersion INTEGER,
+                            ReleaseDate DATE,
+                            NumSongs INTEGER,
+                            PRIMARY KEY (Title, AlbumVersion));
+
+CREATE TABLE Album(AlbumID INTEGER PRIMARY KEY,
+                    Title VARCHAR(20),
+                    AlbumVersion INTEGER,
+                    NumStreams INTEGER,
+                    FOREIGN KEY (Title, AlbumVersion) REFERENCES TitleRelease,
+                    FOREIGN KEY (NumStreams) REFERENCES StreamAlbumCertification);
+
+CREATE TABLE AlbumHasKeyword(KeywordID INTEGER PRIMARY KEY,
+                            Keyword VARCHAR(30),
+                            AlbumID INTEGER,
+                            FOREIGN KEY (AlbumID) REFERENCES Album);
+
+CREATE TABLE AlbumIncludesSong(AlbumID INTEGER NOT NULL,
+                                SongID INTEGER,
+                                PRIMARY KEY (AlbumID, SongID),
+                                FOREIGN KEY (AlbumID) REFERENCES Album,
+                                FOREIGN KEY (SongID) REFERENCES Song);
+
+CREATE TABLE ArtistCreatesSong(UserID INTEGER,
+                                SongID INTEGER,
+                                PRIMARY KEY (UserID, SongID),
+                                FOREIGN KEY (UserID) REFERENCES Artist,
+                                FOREIGN KEY (SongID) REFERENCES Song);
+
+CREATE TABLE ArtistHasAlbum(UserID INTEGER,
+                            AlbumID INTEGER,
+                            PRIMARY KEY (UserID, AlbumID),
+                            FOREIGN KEY (UserID) REFERENCES Artist,
+                            FOREIGN KEY (AlbumID) REFERENCES Album);                                                                                     
---
+++ milestone2tuples.sql
@@ -0,0 +1,165 @@
+INSERT INTO Listener (UserID,Email,ListeningTime,TopGenre) VALUES( 0, 'holyheck@gmail.com', 1359022,'Pop');
+INSERT INTO Listener (UserID,Email,ListeningTime,TopGenre) VALUES( 1, 'reeeee@gmail.com', 300,'Rap');
+INSERT INTO Listener (UserID,Email,ListeningTime,TopGenre) VALUES( 2, 'dolphinsareok@outlook.com', 65123,'K-Pop');
+INSERT INTO Listener (UserID,Email,ListeningTime,TopGenre) VALUES( 52, 'apple@hotmail.com', 5,'Classical');
+INSERT INTO Listener (UserID,Email,ListeningTime,TopGenre) VALUES( 69, 'nice@hotmail.com', 420,'Monkey-Trap');
+
+INSERT INTO Artist (UserID, Email, MonthlyListeners)
+VALUES
+(
+        0,
+        'holyheck@gmail.com',
+        1630928
+    );
+INSERT INTO Artist (UserID, Email, MonthlyListeners)
+VALUES
+    (
+        3,
+        'shandilya@gmail.com',
+        250
+    );
+INSERT INTO Artist (UserID, Email, MonthlyListeners)
+VALUES    (
+        4,
+        'ilovedolphins@outlook.com',
+        1205304
+    );
+INSERT INTO Artist (UserID, Email, MonthlyListeners)
+VALUES(
+        5,
+        'ihatedolphins@hotmail.com',
+        340792
+    );
+INSERT INTO Artist (UserID, Email, MonthlyListeners)
+VALUES(
+        6,
+        'imaboomer@yahoo.com',
+        1
+    );
+
+INSERT INTO ProfileHas (UserID, Nickname)
+VALUES(0, 'Melissa');
+INSERT INTO ProfileHas (UserID, Nickname)
+VALUES (1, 'Rahul');
+INSERT INTO ProfileHas (UserID, Nickname)
+VALUES    (2, 'Mark');
+INSERT INTO ProfileHas (UserID, Nickname)
+VALUES    (52, 'Michelle');
+INSERT INTO ProfileHas (UserID, Nickname)
+VALUES     (69, 'ironcat');
+
+INSERT INTO StreamSongCertification (NumStreams, Certification) VALUES (10000000, 'Diamond');
+INSERT INTO StreamSongCertification (NumStreams, Certification) VALUES (2000000, 'Multi-Platinum');
+INSERT INTO StreamSongCertification (NumStreams, Certification) VALUES (500000, 'Gold');
+INSERT INTO StreamSongCertification (NumStreams, Certification) VALUES (1000000, 'Platinum');
+INSERT INTO StreamSongCertification (NumStreams, Certification) VALUES (100000, 'None');
+
+INSERT INTO LyricsTitle(Lyrics, Title) VALUES
+('Fly','Baby Shark');
+INSERT INTO LyricsTitle(Lyrics, Title) VALUES
+('Never','Never ');
+INSERT INTO LyricsTitle(Lyrics, Title) VALUES
+('Auuuugh','ARRRRRGH');
+INSERT INTO LyricsTitle(Lyrics, Title) VALUES
+('NAAAAAAAAAAAAAH','Trumpet Boiii');
+INSERT INTO LyricsTitle(Lyrics, Title) VALUES
+('Country','Country Road');
+
+INSERT INTO
+    Song (SongID, Genre,Duration,Lyrics,NumStreams)
+VALUES (0, 'Pop', 3, 'Fly', 10000000);
+ 
+INSERT INTO
+    Song (SongID, Genre,Duration,Lyrics,NumStreams)
+VALUES (1, 'Rap', 6, 'Never', 2000000);
+ 
+INSERT INTO Song (SongID, Genre,Duration,Lyrics,NumStreams)
+VALUES (53, 'Monkey-Trap', 4, 'Auuuugh', 500000);
+ 
+INSERT INTO Song (SongID, Genre,Duration,Lyrics,NumStreams)
+VALUES (47, 'Jazz', 1, 'NAAAAAAAAAAAAAH', 1000000);
+ 
+INSERT INTO Song (SongID, Genre,Duration,Lyrics,NumStreams)
+VALUES (14, 'Country', 5, 'Country', 100000);
+
+INSERT INTO UserLikesSong (UserID, SongID)
+VALUES(0, 0);
+INSERT INTO UserLikesSong (UserID, SongID)
+VALUES(0, 1);
+INSERT INTO UserLikesSong (UserID, SongID)
+VALUES (2, 53);
+INSERT INTO UserLikesSong (UserID, SongID)
+VALUES (52, 0);
+INSERT INTO UserLikesSong (UserID, SongID)
+VALUES(69, 0);
+
+INSERT INTO SongHasKeyword (KeywordID, Keyword, SongID) VALUES (0, 'sociopath', 0);
+INSERT INTO SongHasKeyword (KeywordID, Keyword, SongID) VALUES (1, 'apathy', 0);
+INSERT INTO SongHasKeyword (KeywordID, Keyword, SongID) VALUES (2, 'turn', 53);
+INSERT INTO SongHasKeyword (KeywordID, Keyword, SongID) VALUES (3, 'me', 53);
+INSERT INTO SongHasKeyword (KeywordID, Keyword, SongID) VALUES (4, 'on', 53);
+
+INSERT INTO UserPlaylists (PlaylistID) VALUES (0);
+INSERT INTO UserPlaylists (PlaylistID) VALUES (1);
+INSERT INTO UserPlaylists (PlaylistID) VALUES (2);
+INSERT INTO UserPlaylists (PlaylistID) VALUES (3);
+INSERT INTO UserPlaylists (PlaylistID) VALUES (444);
+
+INSERT INTO UserHasUserPlaylists (UserID, PlaylistID) VALUES (0, 0);
+INSERT INTO UserHasUserPlaylists (UserID, PlaylistID) VALUES (0, 1);
+INSERT INTO UserHasUserPlaylists (UserID, PlaylistID) VALUES (2, 2);
+INSERT INTO UserHasUserPlaylists (UserID, PlaylistID) VALUES (52, 3);
+INSERT INTO UserHasUserPlaylists (UserID, PlaylistID) VALUES (52, 444);
+
+INSERT INTO PlaylistContent (ContentID, PlaylistID) VALUES (0, 0);
+INSERT INTO PlaylistContent (ContentID, PlaylistID) VALUES (1, 1);
+INSERT INTO PlaylistContent (ContentID, PlaylistID) VALUES (2, 2);
+INSERT INTO PlaylistContent (ContentID, PlaylistID) VALUES (3, 3);
+INSERT INTO PlaylistContent (ContentID, PlaylistID) VALUES (444, 444);
+    
+INSERT INTO PlaylistContentIncludesSong (ContentID, SongID) VALUES (0, 0);
+INSERT INTO PlaylistContentIncludesSong (ContentID, SongID) VALUES (0, 1);
+INSERT INTO PlaylistContentIncludesSong (ContentID, SongID) VALUES (2, 47);
+INSERT INTO PlaylistContentIncludesSong (ContentID, SongID) VALUES (2, 53);
+INSERT INTO PlaylistContentIncludesSong (ContentID, SongID) VALUES (444, 53);
+    
+INSERT INTO StreamAlbumCertification (NumStreams, Certification) VALUES (10000000, 'Diamond');
+INSERT INTO StreamAlbumCertification (NumStreams, Certification) VALUES (2000000, 'Multi-Platinum');
+INSERT INTO StreamAlbumCertification (NumStreams, Certification) VALUES (500000, 'Gold');
+INSERT INTO StreamAlbumCertification (NumStreams, Certification) VALUES (1000000, 'Platinum');
+INSERT INTO StreamAlbumCertification (NumStreams, Certification) VALUES (100000, 'None');
+
+INSERT INTO TitleRelease(Title, AlbumVersion, ReleaseDate, NumSongs) VALUES ('032 Funk', 1, '01-APR-21', 5);
+INSERT INTO TitleRelease(Title, AlbumVersion, ReleaseDate, NumSongs) VALUES ('30', 1, '16-MAY-20', 10);
+INSERT INTO TitleRelease(Title, AlbumVersion, ReleaseDate, NumSongs) VALUES ('Light Switch', 1, '30-MAR-22', 1);
+INSERT INTO TitleRelease(Title, AlbumVersion, ReleaseDate, NumSongs) VALUES ('Pigeon!', 1, '25-FEB-22', 8);
+INSERT INTO TitleRelease(Title, AlbumVersion, ReleaseDate, NumSongs) VALUES ('ZZZ', 1, '16-NOV-18', 9);
+
+INSERT INTO Album(AlbumID, Title, AlbumVersion, NumStreams) VALUES (0, '032 Funk', 1, 2000000);
+INSERT INTO Album(AlbumID, Title, AlbumVersion, NumStreams) VALUES (1, '30', 1, 10000000);
+INSERT INTO Album(AlbumID, Title, AlbumVersion, NumStreams) VALUES (2, 'Light Switch', 1, 500000);
+INSERT INTO Album(AlbumID, Title, AlbumVersion, NumStreams) VALUES (5, 'Pigeon!', 1, 100000);
+INSERT INTO Album(AlbumID, Title, AlbumVersion, NumStreams) VALUES (99, 'ZZZ', 1, 1000000);
+
+INSERT INTO AlbumHasKeyword (KeywordID, Keyword, AlbumID) VALUES (0, 'Funk', 0);
+INSERT INTO AlbumHasKeyword (KeywordID, Keyword, AlbumID) VALUES (1, 'Celebration', 0);
+INSERT INTO AlbumHasKeyword (KeywordID, Keyword, AlbumID) VALUES (2, 'Incheon Airport Freestyle', 0);
+INSERT INTO AlbumHasKeyword (KeywordID, Keyword, AlbumID) VALUES (3, 'Sad', 1);
+INSERT INTO AlbumHasKeyword (KeywordID, Keyword, AlbumID) VALUES (4, 'Upbeat', 2);
+
+INSERT INTO AlbumIncludesSong (AlbumID, SongID) VALUES (0, 0);
+INSERT INTO AlbumIncludesSong (AlbumID, SongID) VALUES (0, 1);
+INSERT INTO AlbumIncludesSong (AlbumID, SongID) VALUES (1, 47);
+INSERT INTO AlbumIncludesSong (AlbumID, SongID) VALUES (2, 53);
+INSERT INTO AlbumIncludesSong (AlbumID, SongID) VALUES (2, 14);
+    
+INSERT INTO ArtistHasAlbum (UserID, AlbumID) VALUES (0, 0);
+INSERT INTO ArtistHasAlbum (UserID, AlbumID) VALUES (0, 1);
+INSERT INTO ArtistHasAlbum (UserID, AlbumID) VALUES (3, 2);
+INSERT INTO ArtistHasAlbum (UserID, AlbumID) VALUES (4, 5);
+INSERT INTO ArtistHasAlbum (UserID, AlbumID) VALUES (5, 99);
+    
+    
+    
+    
+    
