23a0a522c297905c188c788febfe5199bdc5f0cd
srahul14 <shandilyaholy@gmail.com>
2022-07-30 10:38:44 -0700

    adding delete and insert tags to the page

--- site.php
+++ site.php
@@ -7,9 +7,27 @@
 </head>
 
 <body>
-    <?php
-    echo ('Welcome to our app');
-    ?>
+    <h2>Insert New Data</h2>
+
+    <form method="POST" action="site.php">
+        <!--refresh page when submitted-->
+        <input type="hidden" id="insertQueryRequest" name="insertQueryRequest">
+        Number: <input type="text" name="insNo"> <br /><br />
+        Name: <input type="text" name="insName"> <br /><br />
+
+        <input type="submit" value="Insert" name="insertSubmit"></p>
+    </form>
+
+    <hr />
+
+    <h2>Delete Data</h2>
+
+    <form method="POST" action="site.php">
+        <!-- if you want another page to load after the button is clicked, you have to specify that page in the action parameter -->
+        <input type="hidden" id="resetTablesRequest" name="resetTablesRequest">
+        <p><input type="submit" value="Reset" name="reset"></p>
+    </form>
+
 </body>
 
 </html>
